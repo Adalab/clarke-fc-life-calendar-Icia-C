f5d429eff07275c14ead8a6a4d8c88dfc2ac13a7
Icia <icia83@gmail.com>
2018-03-24 19:20:32 +0100

    add buttons, styles, modify components

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,86 @@
 import React, { Component } from 'react';
 import Form from './Form';
-//import FaceCalendar from './FaceCalendar';
+import FaceCalendar from './FaceCalendar';
+import {Route, Switch} from 'react-router-dom';
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.handleDate = this.handleDate.bind(this);
+    this.handleState = this.handleState.bind(this);
+    this.handleMessage = this.handleMessage.bind(this);
+    this.saveDay = this.saveDay.bind(this);
+
+    this.state = {
+      date: '',
+      state: '',
+      message: '',
+      saveData: [],
+      isLoading: true
+    }
+  }
+
+  componentWillMount(){
+    localStorage.getItem('saveData') &&
+      this.setState({
+        saveData: JSON.parse(localStorage.getItem('saveData')),
+        isLoading: false
+      })
+  }
+
+  componentWillUpdate(nextProps, nextState){
+    let saveData = this.state.saveData;
+    localStorage.setItem('saveData', JSON.stringify(saveData));
+  }
+
+  //catch input values
+  handleDate(date){
+    let dateInput = date.target.value;
+    this.setState({ date: dateInput })
+  }
+  handleState(state){
+    let stateInput = state.target.value;
+    this.setState({ state: stateInput })
+  }
+  handleMessage(msg){
+    let msgInput = msg.target.value;
+    this.setState({ message: msgInput })
+  }
+
+  saveDay(){
+    let dataDate = this.state.saveData;
+
+    dataDate.push({
+      date: this.state.date,
+      state: this.state.state,
+      msg: this.state.message
+    })
+    this.setState({
+      saveData: dataDate
+    })
+  }
+
   render() {
     return (
       <div className="App">
-      <Form />
+        <header className="header"><h1><span>☀</span> ¿Cómo te ha ido el día? </h1></header>
+        <Switch>
+          <Route exact path="/" render= {() =>
+            <Form handleDate = {this.handleDate}
+                  handleState = {this.handleState}
+                  handleMessage = {this.handleMessage}
+                  saveDay = {this.saveDay}
+                  saveData = {this.state.saveData}
+                  state = {this.state.state}
+            /> }
+          />
+          <Route exact path="/calendar" render= {() =>
+            <FaceCalendar saveData={this.state.saveData}
+            /> }
+          />
+        </Switch>
       </div>
     );
   }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,65 +1,7 @@
 import React from 'react';
-import FaceCalendar from './FaceCalendar';
-import {Link, Route, Switch} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 class Form extends React.Component{
-  constructor(props){
-    super(props);
-
-    this.handleDate = this.handleDate.bind(this);
-    this.handleState = this.handleState.bind(this);
-    this.handleMessage = this.handleMessage.bind(this);
-    this.saveDay = this.saveDay.bind(this);
-    //this.paintSmile = this.paintSmile.bind(this);
-
-    this.state = {
-      date: '',
-      state: '',
-      message: '',
-      saveData: [],
-      isLoading: true
-    }
-  }
-
-  componentWillMount(){
-    localStorage.getItem('saveData') &&
-      this.setState({
-        saveData: JSON.parse(localStorage.getItem('saveData')),
-        isLoading: false
-      })
-  }
-
-  componentWillUpdate(nextProps, nextState){
-    let saveData = this.state.saveData;
-    localStorage.setItem('saveData', JSON.stringify(saveData));
-  }
-
-  //catch input values
-  handleDate(date){
-    let dateInput = date.target.value;
-    this.setState({ date: dateInput })
-  }
-  handleState(state){
-    let stateInput = state.target.value;
-    this.setState({ state: stateInput })
-  }
-  handleMessage(msg){
-    let msgInput = msg.target.value;
-    this.setState({ message: msgInput })
-  }
-
-  saveDay(){
-    let dataDate = this.state.saveData;
-
-    dataDate.push({
-      date: this.state.date,
-      state: this.state.state,
-      msg: this.state.message
-    })
-    this.setState({
-      saveData: dataDate
-    })
-  }
 
   render(){
     return(
@@ -67,36 +9,29 @@ class Form extends React.Component{
         <form  action="">
           <div className="form__date">
             <label htmlFor="date">Fecha</label>
-            <input type="date" name="date" onChange={this.handleDate}/>
+            <input type="date" name="date" onChange={this.props.handleDate}/>
           </div>
           <div className="form__state">
             <p>Estado</p>
             <div className="form__state--check">
-              <input type="checkbox" value=":)" name="happy" onClick={this.handleState} />
+              <input type="radio" value=":)" name="state" onClick={this.props.handleState} />
               <label htmlFor="state">:)</label>
-              <input type="checkbox" value=":(" name="sad" onClick={this.handleState}/>
+              <input type="radio" value=":(" name="state" onClick={this.props.handleState}/>
               <label htmlFor="state">:(</label>
             </div>
           </div>
-          <div className="form__msg">
+          <div className={`form__msg ${ this.props.state === ':)'? 'visible' : 'hidden' }`}>
             <label htmlFor="message">Mensaje</label>
-            <input placeholder="¿Por qué es un buen día?" type="text" name="message" onChange={this.handleMessage} />
+            <input placeholder="¿Por qué es un buen día?" type="text" name="message" onChange={this.props.handleMessage} />
           </div>
         </form>
 
         <div className="form__btn">
           <Link className="link" to='/calendar'>
-            <button className="form__btn--style" onClick={this.saveDay}>Guardar</button>
+            <button className="form__btn--style" onClick={this.props.saveDay}>Guardar</button>
           </Link>
           <button className="form__btn--style">Cancelar</button>
         </div>
-
-        <Switch>
-          <Route exact path="/calendar" render= {() =>
-            <FaceCalendar saveData={this.state.saveData}
-            /> }
-          />
-        </Switch>
       </div>
     );
   }
